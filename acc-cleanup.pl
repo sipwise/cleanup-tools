ebc986df91d127f12af7d3f58440a59136430dc6
Rene Krenn <rkrenn@sipwise.com>
2017-08-07 16:40:53 +0200

    TT#19706 refactor to day-based processing of months
    + processing records of a month is now done day-by-day,
      so the script will effectively distribute load, when
      registered as a daily cron job.
    + handle 'from_unixtime(colname)' explicitly, to hit
      index properly. essential speed up of "backup" mode.
      - no changes to acc-cleanup.conf/.tt needed
    + fix month interval date calculations to make it safe
      to run it on any day - in particular this means to get rid
      of avg days per month (30.3475).
      todo: introduce DateTime.
    
    Change-Id: I39744be3f25c7211a8d54d61a1c7a219181a5847

--- acc-cleanup.pl
+++ acc-cleanup.pl
@@ -16,11 +16,107 @@ my $config_file = "/etc/ngcp-cleanup-tools/acc-cleanup.conf";
 
 my (%vars, $dbh);
 
-sub delete_loop {
-	my ($table, $mtable, $col, $mstart) = @_;
+use constant TIME_COL_TRANSFORMATION_NONE => {
+		value => sub {
+			my ($value,@args) = @_;
+			return $value;
+		},
+		sql_rhs =>	sub {
+			my ($col_name,@args) = @_;
+			return sprintf('%s',$col_name);
+		},
+		sql_lhs =>	sub {
+			my ($col_name,@args) = @_;
+			return sprintf('%s',$col_name);
+		},
+	};
+use constant TIME_COL_TRANSFORMATION_FROM_UNIXTIME => {
+		value => sub {
+			my ($value,@args) = @_;
+			return $value;
+		},
+		sql_rhs =>	sub {
+			my ($col_name,@args) = @_;
+			return sprintf('unix_timestamp(%s)',$col_name);
+		},
+		sql_lhs =>	sub {
+			my ($col_name,@args) = @_;
+			return sprintf('from_unixtime(%s)',$col_name);
+		},
+	};
 
-	my $limit = '';
-	$vars{batch} and $vars{batch} > 0 and $limit = " limit $vars{batch}";
+sub _get_time_column {
+	my ($col,$transformation,$sql_rhs_args,$sql_lhs_args,$value_args) = (undef,undef);
+	my $var_name = "time-column";
+	my $col_name_re = '[a-z0-9_-]+';
+	# from_unixtime(x):
+	if ($vars{$var_name} =~ /^\s*from_unixtime\(\s*($col_name_re)\s*\)\s*$/i) {
+		$col = $1;
+		$transformation = TIME_COL_TRANSFORMATION_FROM_UNIXTIME;
+		$sql_rhs_args = []; $sql_lhs_args = []; $value_args = [];
+	# add other supported syntax here. eg.
+	#  date(x)
+	#  date_sub(x INTERVAL y day), etc.
+	# raw column name:
+	} elsif ($vars{$var_name} =~ /^\s*($col_name_re)\s*$/i) {
+		$col = $1;
+		$transformation = TIME_COL_TRANSFORMATION_NONE;
+		$sql_rhs_args = []; $sql_lhs_args = []; $value_args = [];
+	} else {
+		die("Variable $var_name must show a column name, or an expression to convert to datetime (supported: 'from_unixtime(column name)')");
+	}
+	return {
+		expression => $vars{$var_name},
+		column_name => $col,
+		transformation => $transformation,
+		value_args => $value_args,
+		sql_rhs_args => $sql_rhs_args,
+		sql_lhs_args => $sql_lhs_args,
+	};
+}
+
+sub _get_transformed_sql_rhs {
+	my ($col,$literal) = @_;
+	return &{$col->{transformation}->{sql_rhs}}($literal,@{$col->{sql_rhs_args}});
+}
+
+sub _get_transformed_sql_lhs {
+	my ($col,$literal) = @_;
+	return &{$col->{transformation}->{sql_lhs}}($literal,@{$col->{sql_lhs_args}});
+}
+
+sub _get_transformed_value {
+	my ($col,$value) = @_;
+	return &{$col->{transformation}->{value}}($value,@{$col->{value_args}});
+}
+
+sub _delete_loop {
+
+	my ($table, $col, $start, $stop) = @_;
+	my $stmt = "delete from $table where ";
+
+	my @params = ();
+	$stmt .= "$col->{column_name} >= " . _get_transformed_sql_rhs($col,'?');
+	push(@params,_get_transformed_value($col,$start));
+	$stmt .= " and $col->{column_name} < " . _get_transformed_sql_rhs($col,'?');
+	push(@params,_get_transformed_value($col,$stop));
+
+	$stmt .= " limit $vars{batch}" if $vars{batch} and $vars{batch} > 0;
+
+	my $total = 0;
+	while (1) {
+		my $size = $dbh->do($stmt,undef, @params)
+			or die("Unable to delete records from $table: " . $DBI::errstr);
+		$total += $size;
+		last unless $size > 0;
+	}
+	return $total;
+
+}
+
+sub _move_loop {
+
+	my ($table, $mtable, $col, $start, $stop) = @_;
 
 	my $sth = $dbh->prepare("show fields from $table");
 	$sth->execute;
@@ -37,13 +133,20 @@ sub delete_loop {
 
 	my $primary_key_cols = join(",",@keycols);
 
-	#$mstart = '2016-12-01 00:00:00';
+	my $temp_table = $table . "_tmp";
+	my $stmt = "select $primary_key_cols from $table where ";
 
+	my @params = ();
+	$stmt .= "$col->{column_name} >= " . _get_transformed_sql_rhs($col,'?');
+	push(@params,_get_transformed_value($col,$start));
+	$stmt .= " and $col->{column_name} < " . _get_transformed_sql_rhs($col,'?');
+	push(@params,_get_transformed_value($col,$stop));
+
+	$stmt .= " limit $vars{batch}" if $vars{batch} and $vars{batch} > 0;
+
+	my $total = 0;
 	while (1) {
-		my $temp_table = $table . "_tmp";
-		my $size = $dbh->do("create temporary table $temp_table as ".
-		        "(select $primary_key_cols from $table " .
-				"where $col >= ? and $col < date_add(?, interval 1 month) $limit)",undef, $mstart, $mstart)
+		my $size = $dbh->do("create temporary table $temp_table as ($stmt)",undef, @params)
 			or die("Failed to create temporary table $temp_table: " . $DBI::errstr);
 		if ($size > 0) {
 			$dbh->do("insert into $mtable select s.* from ".
@@ -54,24 +157,26 @@ sub delete_loop {
 		}
 		$dbh->do("drop temporary table $temp_table")
 			or die("Failed to drop temporary table $temp_table: " . $DBI::errstr);
+		$total += $size;
 		last unless $size > 0;
 	}
+	return $total;
+
 }
 
 sub archive_dump {
 	my ($table) = @_;
 
 	my $month = $vars{"archive-months"};
+	my @now = localtime(time());
 	while (1) {
-		my $now = time();
-		my $bt = $now - int(30.4375 * 86400 * $month);
-		my @bt = localtime($bt);
-		my $mtable = $table . "_" . sprintf('%04i%02i', $bt[5] + 1900, $bt[4] + 1);
+		my ($m,$y) = _add_months($now[4] + 1, $now[5] + 1900, -1 * $month);
+		my $mtable = $table . "_" . sprintf('%04i%02i', $y, $m);
 		my $res = $dbh->selectcol_arrayref("show table status like ?", undef, $mtable);
 		($res && @$res && $res->[0]) or last;
 		$month++;
 		if ($vars{"archive-target"} ne '/dev/null') {
-			my $target = $vars{"archive-target"} . "/$mtable." . sprintf('%04i%02i%02i%02i%02i%02i', $bt[5] + 1900, $bt[4] + 1, @bt[3,2,1,0]) . ".sql";
+			my $target = $vars{"archive-target"} . "/$mtable." . sprintf('%04i%02i%02i%02i%02i%02i', $now[5] + 1900, $now[4] + 1, @now[3,2,1,0]) . ".sql";
 
 			my @cmd = ('mysqldump');
 			$vars{username} and push(@cmd, "-u" . $vars{username});
@@ -100,17 +205,28 @@ sub archive_dump {
 sub backup_table {
 	my ($table) = @_;
 
-	for my $cmonth (0 .. ($vars{"backup-retro"} - 1)) {
-		my $tmonths = $cmonth + $vars{"backup-months"};
-		my $bt = time() - int(30.4375 * 86400 * $tmonths);
-		my @bt = localtime($bt);
-		my $tstampl = sprintf('%04i-%02i', $bt[5] + 1900, $bt[4] + 1);
-		my $tstamp = sprintf('%04i%02i', $bt[5] + 1900, $bt[4] + 1);
-		my $mstart = "$tstampl-01 00:00:00";
+	my $col = _get_time_column();
 
-		my $mtable = $table . "_$tstamp";
+	my @range = ();
+	my @now = localtime(time());
+	foreach my $cmonth (($vars{"backup-retro"} - 1,0)) {
+		my $tmonths = $cmonth + $vars{"backup-months"};
+		my ($m,$y) = _add_months($now[4] + 1, $now[5] + 1900, -1 * $tmonths);
+		push(@range,[$y,$m,_days_of_month($m,$y)]);
+	}
+	my $date = sprintf('%04i-%02i-01', $range[0][0], $range[0][1]);
+	my $stop = sprintf('%04i-%02i-%02i', $range[1][0], $range[1][1], $range[1][2]);
+	my $next;
+	my $time = '00:00:00';
+	while (($date cmp $stop) <= 0) {
+		my ($y,$m,$d) = _split_date($date);
+		my $mtable = $table . "_$y$m";
 		$dbh->do("create table if not exists $mtable like $table");
-		delete_loop($table, $mtable, $vars{"time-column"}, $mstart);
+		$next = _add_days($date,1);
+		my $total = _move_loop($table, $mtable, $col, "$date $time", "$next $time");
+		#print "$date $time - $next $time: $total records moved from $table to $mtable\n";
+	} continue {
+		$date = $next;
 	}
 
 	return 1;
@@ -119,16 +235,194 @@ sub backup_table {
 sub cleanup {
 	my ($table) = @_;
 
-	my $limit = '';
-	$vars{batch} and $vars{batch} > 0 and $limit = " limit $vars{batch}";
-	my $col = $vars{"time-column"};
+	my $col = _get_time_column();
 
-	while (1) {
-		my $aff = $dbh->do("delete from $table where $col < date(date_sub(now(), interval ? day)) $limit",
-			undef, $vars{"cleanup-days"});
-		$aff or die("Unable to delete records from $table");
-		$aff == 0 and last;
+	my $sth = $dbh->prepare('select ' . _get_transformed_sql_lhs($col,"min($col->{column_name})") . " from $table");
+	$sth->execute;
+	my ($min_time) = $sth->fetchrow_array();
+	$sth->finish;
+
+	return unless $min_time;
+
+	(my $date,undef) = _split_datetime($min_time);
+	my $t = time() - int(86400 * $vars{"cleanup-days"});
+	my @t = localtime($t);
+	my $stop = sprintf('%04i-%02i-%02i', $t[5] + 1900, $t[4] + 1, $t[3]);
+	my $stop_time = sprintf('%02i:%02i:%02i', $t[2], $t[1], $t[0]);
+
+	my $next;
+	my $time = '00:00:00';
+	my $total;
+	while (($date cmp $stop) < 0) {
+		$next = _add_days($date,1);
+		$total = _delete_loop($table, $col, "$date $time", "$next $time");
+		#print "$date $time - $next $time: $total records deleted from $table\n";
+	} continue {
+		$date = $next;
 	}
+	$total = _delete_loop($table, $col, "$date $time", "$date $stop_time");
+	#print "$date $time - $date $stop_time: $total records deleted from $table\n";
+
+}
+
+# date manip helper methods. todo: port to DateTime.
+
+sub _add_days {
+
+	my ($date,$ads) = @_;
+
+	my ($year,$month,$day) = _split_date($date);
+
+	my $rday = $day;
+	my $rmonth = $month;
+	my $ryear = $year;
+
+	my $result;
+
+	if($ads >= 0) { # addition
+		for (1 .. $ads) {
+			# increment day, turn month forward:
+			if ($rday < _days_of_month($rmonth,$ryear)) {
+				$rday++;
+			} else {
+				$rmonth++;
+				$rday = 1;
+			}
+			# turn year forward:
+			if ($rmonth > 12) {
+				$rday = 1;
+				$rmonth = 1;
+				$ryear++;
+			}
+		}
+	} else { # difference
+		my $subs = -1 * $ads;
+		for (1 .. $subs) {
+			# decrement day, turn month backward
+			if ($rday > 1) {
+				$rday--;
+			} else {
+				$rmonth--;
+				$rday = _days_of_month($rmonth,$ryear);
+			}
+			# turn year backward:
+			if ($rmonth < 1) {
+				$rmonth = 12;
+				$rday = _days_of_month($rmonth,$ryear);
+				$ryear--;
+			}
+		}
+	}
+
+	return $ryear . '-' . _zerofill($rmonth,2) . '-' . _zerofill($rday,2);
+
+}
+
+sub _add_months {
+
+	my ($month, $year, $ads) = @_;
+
+	if ($month > 0  and $month <= 12) {
+
+		my $sign = ($ads > 0) ? 1 : -1;
+		my $rmonths = $month + $sign * (abs($ads) % 12);
+		my $ryears = $year + int( $ads / 12 );
+
+		if ($rmonths < 1) {
+			$rmonths += 12;
+			$ryears -= 1;
+		} elsif ($rmonths > 12) {
+			$rmonths -= 12;
+			$ryears += 1;
+		}
+
+		return ($rmonths,$ryears);
+
+	} else {
+
+		return (undef,undef);
+
+	}
+
+}
+
+sub _split_date {
+
+	my $datestring = shift;
+	return split /-/,$datestring,3;
+
+}
+
+sub _split_time {
+
+	my $timestring = shift;
+	return split /:/,$timestring,3;
+
+}
+
+sub _split_datetime {
+
+	my $timestampstring = shift;
+	return split / /,$timestampstring,2;
+
+}
+
+sub _split_timestamp {
+
+	my $timestampstring = shift;
+	my ($date,$time) = _split_datetime($timestampstring);
+	my ($year,$month,$day) = _split_date($date);
+	my ($hour,$minute,$second) = _split_time($time);
+	return ($year,$month,$day,$hour,$minute,$second);
+
+}
+
+sub _zerofill {
+	my ($i,$d) = @_;
+	my $z = $d - length($i);
+	my $res = $i;
+	if ($d > 0) {
+		foreach (1 .. $z) {
+			$res = '0' . $res;
+		}
+	}
+	return $res;
+}
+
+sub _days_of_month {
+
+	my ($month, $year) = @_;
+	if ($month > 0  and $month <= 12) {
+		if ($month == 2 and _is_leapyear($year)) { # leapyear
+			return 29;
+		} else {
+			my @daysofmonths = (31,28,31,30,31,30,31,31,30,31,30,31);
+			return $daysofmonths[$month - 1];
+		}
+	} else {
+		return 0;
+	}
+
+}
+
+sub _is_leapyear {
+
+	my $year = shift;
+	my $v = 0;
+	if (!$year) {
+		return -1;
+	}
+	if ($year % 4 == 0) {
+		$v = 1;
+	}
+	if ($year % 100 == 0) {
+		$v = 0;
+	}
+	if ($year % 400 == 0) {
+		$v = 1;
+	}
+	return $v;
+
 }
 
 ########################################################################
